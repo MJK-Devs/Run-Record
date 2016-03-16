affdd0b3ac9f3911286bd0361a911d7aaccf6abd
Kevin Novak <kevinmnovak@yahoo.com>
2016-03-16 01:50:50 -0400

    Created Database Connection Function
    Used to execute a query and return an array. See sample usage at bottom
    of file. Not sure if this is the best method. Probably will need
    revised.

---
+++ Project/db.php
@@ -0,0 +1,45 @@
+<?php
+// =======================================
+// Execute Query
+// =======================================
+// function that returns an array of values from database
+// see sample usage below
+function executeQuery($query, $attribute) {
+    try {
+        // connect to database
+        $connString = "mysql:host=localhost;dbname=knovak18";
+        $user = "knovak18";
+        $pass = "web2";
+
+        $pdo = new PDO($connString,$user,$pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // create query
+        $result = $pdo->query($query);
+
+        // put query results into array
+        $array = array();
+        while ($row = $result->fetch()) {
+
+            // selects the row based on the pased attribute
+            $array[] = $row[$attribute];
+        }
+        $pdo = null;
+
+        // return the results
+        return $array;
+    }
+    catch (PDOException $e) {
+        die( $e->getMessage() );
+        return null;
+    }
+}
+
+// =======================================
+// Sample Usage:
+// =======================================
+// $artists = executeQuery("select * from artists", "LastName");
+// foreach($artists as $value) {
+//     echo $value . '<br/>';
+// }
+?>
