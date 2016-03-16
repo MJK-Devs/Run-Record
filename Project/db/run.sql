4841d38cf129432e00b11661a4c049a3188ff04f
Kevin Novak <kevinmnovak@yahoo.com>
2016-03-16 16:19:02 -0400

    Created user table

---
+++ Project/db/run.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 20, 2013 at 03:45 AM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `run`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rr-user`
+--
+DROP TABLE IF EXISTS `rr-user`;
+CREATE TABLE IF NOT EXISTS `rr-user` (
+    `UserID` int(11) NOT NULL AUTO_INCREMENT,
+    `Username` varchar(50) NOT NULL,
+    `Password` varchar(50) NOT NULL,
+    `Email` varchar(50) NOT NULL,
+    `FirstName` varchar(50) NOT NULL,
+    `LastName` varchar(50) NOT NULL,
+    `DOB` date DEFAULT NULL,
+    `Gender` char DEFAULT NULL,
+    `Weight` int(11) DEFAULT '0',
+    `JoinDate` date DEFAULT NULL,
+    PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=122 ;
+
+--
+-- Dumping data for table `rr-user`
+--
+
+INSERT INTO `rr-user` (`UserID`, `Username`, `Password`, `Email`, `FirstName`, `LastName`, `DOB`, `Gender`, `Weight`, `JoinDate`) VALUES
+    (2, "knovak18", "web2", "knovak18@kent.edu", "Kevin", "Novak", "1993-11-28", "M", 140, "2016-03-16");
+
+-- --------------------------------------------------------
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
