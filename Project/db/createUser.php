40f12b227edb08276d8aa0e945591084926fdd8a
Kevin Novak <kevinmnovak@yahoo.com>
2016-03-16 22:41:26 -0400

    Form now creates user in DB.
    Only if all required fields are filled.

--- Project/db/createUser.php
+++ Project/db/createUser.php
@@ -1,11 +1,10 @@
 <?php
 include_once 'db.php';
-echo $_POST['username'] . '<br/>';
-echo $_POST['password'] . '<br/>';
-echo $_POST['email'] . '<br/>';
-echo $_POST['firstName'] . '<br/>';
-echo $_POST['lastName'] . '<br/>';
-echo $_POST['male'] . '<br/>';
-echo $_POST['female'] . '<br/>';
-echo $_POST['dateOfBirth'] . '<br/>';
+$gender = null;
+if ($_POST['male'] == "on") {
+    $gender = 'M';
+} else if ($_POST['female'] == "on") {
+    $gender = 'F';
+}
+createUser($_POST['username'], $_POST['password'], $_POST['email'], $_POST['firstName'], $_POST['lastName'], $_POST['dateOfBirth'], $gender);
 ?>
