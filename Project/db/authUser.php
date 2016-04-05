c96d88e0d746fef1e959dff062434098ff844ea9
Matt Boehlke <matthew.boehlke@gmail.com>
2016-04-05 19:02:38 -0400

    Login Fixes
    No longer authenticating with empty username or password fields.

--- Project/db/authUser.php
+++ Project/db/authUser.php
@@ -10,7 +10,7 @@
 		print($username);
 		print($password);
 
-		if(authUser($username, $pass)) {
+		if(authUser($username, $pass) && !empty($_POST['username']) && !empty($_POST['password'])) {
 			setcookie("User", $username, time() + (86400 * 30), "/"); // 86400 = 1 day
 			setcookie("loginError", "", time() - 3600, "/"); //reset any login errors
 			header('Location: https://webdev.cs.kent.edu/~mboehlke/Web2/RR/main.php');
