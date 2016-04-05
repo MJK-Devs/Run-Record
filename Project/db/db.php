dafad2c571ab48e88087290fc1ea4f18e9e7395a
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-05 19:03:19 -0400

    Fixed authenticate user function.

--- Project/db/db.php
+++ Project/db/db.php
@@ -46,6 +46,7 @@ function executeQuery($query, $attribute) {
 // Create User
 // =======================================
 // creates a user in the database based on the passed parameters
+// Sample Usage: createUser("knovak19", "web3", "knovak19@kent.edu", "Kevin", "Novak", "1993-11-29", "M");
 function createUser($username, $password, $email, $firstname, $lastname, $dob, $gender) {
     try {
         // connect to database
@@ -77,17 +78,44 @@ function createUser($username, $password, $email, $firstname, $lastname, $dob, $
 // Authenticate User
 // =======================================
 // Checks if a user with the provided password exists in the database
-// returns TRUE if yes
+// returns true if yes
+// Sample Usage: authUser("knovak18", "web2");
 function authUser($username, $password) {
-    $pass = executeQuery("SELECT * FROM rruser WHERE Username ='".$username."'", "Password");
-    if(strcmp($pass[0], $password) == 0) {
-        return true;
+    try {
+        $connString = "mysql:host=localhost;dbname=knovak18";
+        $user = "knovak18";
+        $pass = "web2";
+
+        $pdo = new PDO($connString,$user,$pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = "select * from rruser where Username=\"" . $username . "$sql = "select * from rruser where Username=\"" . $username . "\"";"";
+        $result = $pdo->query($sql);
+
+        while ($row = $result->fetch()) {
+            if($row['Password'] == $password) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        $pdo = null;
     }
-    return false;
+    catch (PDOException $e) {
+        die( $e->getMessage() );
+    }
 }
 
-// // Create User - Sample Usage:
-// createUser("knovak19", "web3", "knovak19@kent.edu", "Kevin", "Novak", "1993-11-29", "M");
+// =======================================
+// OLD Authenticate User
+// =======================================
+// function authUser($username, $password) {
+//     $pass = executeQuery("SELECT * FROM rruser WHERE Username ='".$username."'", "Password");
+//     if(strcmp($pass[0], $password) == 0) {
+//         return true;
+//     }
+//     return false;
+// }
 
 // authenticates a user. returns true if the user authenticated
 // and false otherwise
@@ -102,7 +130,7 @@ function authUser($username, $password) {
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 		$query = "SELECT * FROM rrusers";
-		
+
         // create query
         $result = $pdo->query($query);
 
@@ -110,19 +138,19 @@ function authUser($username, $password) {
         $array = array();
         $authenticated = false;
 		print_r($users);
-	
+
 		// finds the username & password in the database
 		foreach($users as $user) {
 			if(strcmp($username, $user["username"]) === 0){
 				if(strcmp($password, $user["password"]) === 0) {
 					// credentials matched, so returns true
 					$authenticated = true;
-				}	
+				}
 			}
 		}
-		
+
 		$pdo = null;
-	
+
 		return $authenticated;
 
     }
