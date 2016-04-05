9deee461ddced3e54ab25a78a13892483d3d6dd3
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-05 19:14:43 -0400

    Relative paths instead of hard-coded ones.

--- Project/db/authUser.php
+++ Project/db/authUser.php
@@ -6,18 +6,18 @@
 
 		$username = $_POST['username'];
 		$pass = $_POST['password'];
-		
+
 		print($username);
 		print($password);
 
 		if(authUser($username, $pass) && !empty($_POST['username']) && !empty($_POST['password'])) {
 			setcookie("User", $username, time() + (86400 * 30), "/"); // 86400 = 1 day
 			setcookie("loginError", "", time() - 3600, "/"); //reset any login errors
-			header('Location: https://webdev.cs.kent.edu/~mboehlke/Web2/RR/main.php');
+			header('Location: ../main.php');
 		}
 		else {
 			setcookie("loginError", "password", time() + (30), "/");
-			header('Location: https://webdev.cs.kent.edu/~mboehlke/Web2/RR/login.php');
+			header('Location: ../login.php');
 		}
 	}
 ?>
