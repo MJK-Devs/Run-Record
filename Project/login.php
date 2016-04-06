13898bf1bbeb2decfd1bcd5121735ed65621935a
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-06 16:02:09 -0400

    Cleaned up login code. Moved PHP to bottom.

--- Project/login.php
+++ Project/login.php
@@ -1,68 +1,34 @@
 <?php
-
 	include_once "db/db.php";
 
 	// DEBUG - Comment the below for testing purposes
+<?php
+function checkCookie() {
 	if(isset($_COOKIE['User'])) {
 		//redirect user to main page if they are already authenticated.
 		header('Location: https://webdev.cs.kent.edu/~mboehlke/Web2/RR/main.php');
 	}
+}
 
-	function formPassword() {
-		if(isset($_COOKIE['loginError'])) {
-			if(strcmp($_COOKIE['loginError'],"password") == 0) {
-				print('
-					<div class="form-group has-error">
-                 	<label for="exampleInputPassword2">Password</label>
-                  	<input type="password" class="form-control" name="password">
-                  	</div>'
-                );
-				setcookie("loginError", "", time() - 3600, "/");
-			}
-		}
-		else {
+function formPassword() {
+	if(isset($_COOKIE['loginError'])) {
+		if(strcmp($_COOKIE['loginError'],"password") == 0) {
 			print('
-					<div class="form-group">
-                  	<label for="exampleInputPassword2">Password</label>
-                  	<input type="password" class="form-control" name="password">
-                  	</div>'
-                );
+				<div class="form-group has-error">
+				<label for="exampleInputPassword2">Password</label>
+				<input type="password" class="form-control" name="password">
+				</div>'
+			);
+			setcookie("loginError", "", time() - 3600, "/");
 		}
 	}
-
-?>
-
-<head> <title>Record Run Login</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<link href="style.css" rel="stylesheet">
-</head>
-
-<body>
-<div class="header">
-	<img width="100" height="100" alt="" src="images/logo.png">
-	<h1><b>Login</b></h1>
-</div>
-
-<div class="container">
-	<div class="row">
-	</div>
-	<div class="col-md-6">
-		<div id="login">
-
-			<form role="form" action="db/authUser.php" method="post">
+	else {
+		print('
 				<div class="form-group">
-					<label for="usernameInput">Username</label>
-					<input type="text" class="form-control" name="username">
-				</div>
-				<?php
-					formPassword();
-				?>
-              <button type="submit" value="submit" class="btn btn-primary">Login</button>
-            </form>
-         </div>
-      </div>
-      <div class="col-md-6">
-      </div>
-   </div>
-</div>  <!-- end container -->
-</body>
+				<label for="exampleInputPassword2">Password</label>
+				<input type="password" class="form-control" name="password">
+				</div>'
+			);
+	}
+}
+?>
