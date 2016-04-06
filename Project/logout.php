a7df85b7504873906c922a2b2137a33a9340101f
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-05 22:48:52 -0400

    Changed logout to relative path.

--- Project/logout.php
+++ Project/logout.php
@@ -2,5 +2,5 @@
 // set the expiration date to one hour ago
 setcookie("User", "", time() - 3600, "/");
 setcookie("loginError", "", time() - 3600, "/"); // reset any login errors
-header('Location: https://webdev.cs.kent.edu/~mboehlke/Web2/RR/login.php');
+header('Location: ./login.php');
 ?>
