75230ed8259cb5b82c8a11d2de9ebbc3010557f2
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-05 21:10:35 -0400

    Simplified and commented.

--- Project/db/createUser2.php
+++ Project/db/createUser2.php
@@ -2,6 +2,7 @@
 session_start();
 
 if (isset($_POST['username'])) {
+    // checks if username is empty
     if (empty($_POST['username'])) {
         $_SESSION['failedUsername'] = "Username cannot be empty!";
     }
