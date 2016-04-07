aa87ab4873788d7960a70681501a402ce879cfb6
jaason435 <jaason435@gmail.com>
2016-04-06 20:39:57 -0400

    Added tags to time form

--- Project/myruns.php
+++ Project/myruns.php
@@ -11,14 +11,16 @@
 <?php include("includes/navbar.php"); ?>
 
 
-<br>
-<br>
-<h3><b>General Run Information</b></h3>
-<br>
 
 <div class="container">
 	<div class="row">
 	<div class="col-md-10">
+		
+		<br>
+		<br>
+		<h3><b>General Run Information</b></h3>
+		<br>
+
 		<div id="record-a-run">
 			<div class="form-inline" method="post">
 				<label>Date</label>
@@ -62,32 +64,35 @@ function printTimeForms() {
 	$seconds = 0;
 	
 	//print hour options
-	echo '<select name="hour" class="form-control"><label>h</label>';
+	echo '<select name="hour" class="form-control">';
 	while($hours <= 12) {
 		echo '<option>' . $hours . '</option>'; 
 		$hours = $hours + 1;
 		echo $hours;
 	}
 	echo '</select>';
+	echo '<label> &nbsp; h &nbsp; </label>';
 	
 	//print minute options
-	echo '<select name="minutes" class="form-control"><label>m</label>';
+	echo '<select name="minutes" class="form-control">';
 	while($minutes <= 60) {
 		echo '<option>' . $minutes . '</option>'; 
 		$minutes = $minutes + 1;
 		echo $minutes;
 	}
 	echo '</select>';
+	echo '<label> &nbsp; m &nbsp; </label>';
 	
 	//print second options
 	echo '<select name="seconds" class="form-control">';
-	echo '<label>s</label>';
+	
 	while($seconds <= 60) {
 		echo '<option>' . $seconds . '</option>'; 
 		$seconds = $seconds + 1;
 		echo $seconds;
 	}
 	echo '</select>';
+	echo '<label> &nbsp; s</label>';
 
 }
 ?>
