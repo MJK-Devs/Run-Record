351d9196499178044655c44a8d90f7af8250899b
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-23 18:43:33 -0400

    Fixed some broken links in stats version of navbar.

--- Project/stats/navbar.php
+++ Project/stats/navbar.php
@@ -11,7 +11,7 @@
 		</div>
 		<div id="navbarCollapse" class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
-				
+
 				<li class="active"><a href="../main.php">Home</a></li>
 				<li class="dropdown">
 					<a data-toggle="dropdown" class="dropdown-toggle" role="button"
@@ -29,12 +29,12 @@
 			<?php
 			print('<ul class="nav navbar-nav navbar-right">');
 					if(isset($_COOKIE['User'])) {
-						print('<li><a href="profile.php">'.$_COOKIE['User'].'</a></li>');
+						print('<li><a href="../profile/profile.php">'.$_COOKIE['User'].'</a></li>');
 						print('<li><a href="../logout.php">Log Out</a></li>');
 					}
 					else {
-						print('<li><a href="login.php#">Login</a></li>');
-						print('<li><a href="create.php">Create Account</a></li>');
+						print('<li><a href="../login.php#">Login</a></li>');
+						print('<li><a href="../create.php">Create Account</a></li>');
 					}
 				?>
 				<!-- if the user is logged in, replace create account with
