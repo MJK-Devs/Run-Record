a9aebd545f3de482539e06bf579ad120ac5dc605
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-24 18:12:29 -0400

    Fixed navbar.

--- Project/includes/navbar.php
+++ Project/includes/navbar.php
@@ -11,7 +11,7 @@
 		</div>
 		<div id="navbarCollapse" class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
-				
+
 				<li class="active"><a href="main.php" class="logo navbar-brand">Record•Run</a></li>
 				<li class="dropdown">
 					<a data-toggle="dropdown" class="dropdown-toggle" role="button"
