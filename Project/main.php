053d19249d39308edd3a28fd47b277a4b7157b07
Kevin Novak <kevinmnovak@yahoo.com>
2016-04-24 22:32:11 -0400

    Modular header on main.php

--- Project/main.php
+++ Project/main.php
@@ -1,8 +1,5 @@
 <head>  <title>Record Run</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-	<link href="css/cosmo.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<link href='https://fonts.googleapis.com/css?family=Bangers&text=Record%E2%80%A2Run' rel='stylesheet' type='text/css'>
+	<?php include("includes/header.php"); ?>
 <!--<script src="includes/sliderScript.js"></script>-->
 <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">-->
 </head>
