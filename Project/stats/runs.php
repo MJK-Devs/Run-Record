eb40ec70bd37bc9db9339df6ca59bea8eea47bc6
Kevin Novak <kevinmnovak@yahoo.com>
2016-05-01 19:18:46 -0400

    Fixed ordering of runs in My Runs.

--- Project/stats/runs.php
+++ Project/stats/runs.php
@@ -24,28 +24,28 @@ class UserRuns {
 	private $UserID;
 	private $runs = array();
 	private $t;
-	
+
 	function __construct($ID){
 		try {
 
 			$connString = "mysql:host=localhost;dbname=knovak18";
 			$user = "knovak18";
 			$pass = "web2";
-			
+
 			$pdo = new PDO($connString,$user,$pass);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$sql = "SELECT RunID, Date, Distance, Time, TimeOfDay, Difficulty, Terrain, Conditions, Temperature, Comments FROM rruser 
+			$sql = "SELECT RunID, Date, Distance, Time, TimeOfDay, Difficulty, Terrain, Conditions, Temperature, Comments FROM rruser
 					NATURAL JOIN rruserruns NATURAL JOIN rrruns WHERE
-					UserID=" . $ID . "";
-					
+					UserID=" . $ID . " ORDER BY Date DESC, AddDate DESC";
+
 			$result = $pdo->query($sql);
 			$r = $result->fetchAll();
-			
+
 			date_default_timezone_set('America/New_York');
 
 			$this->UserID = getUserID($_COOKIE['User']);
 			$user = new User($this->UserID);
-			
+
 			foreach($r as $run) {
 				// grab data from database for this run
 				$RunID = $run["RunID"];
@@ -59,7 +59,7 @@ class UserRuns {
 				$timeOfDay = $run["TimeOfDay"];
 				$comments = $run["Comments"];
 				$caloriesBurned = calculateCaloriesBurned($user->getAge(), $user->getWeight(), $user->getGender(), $time);
-				
+
 				// create new Run object and add it to $this->runs array
 				$thisRun = new Run($RunID, $date, $distance, $time, $caloriesBurned, $terrain, $difficulty, $conditions, $temperature, $timeOfDay, $comments );
 				$this->runs[] = $thisRun;
@@ -68,22 +68,22 @@ class UserRuns {
 				$this->TotalNumberOfRuns = $this->TotalNumberOfRuns + 1;
 				$this->TotalTime = $this->TotalTime + $time;
 				$this->TotalDistance = $this->TotalDistance + $distance;
-				
+
 				// calculate calories
 				if($caloriesBurned == -1) { $this->TotalCaloriesBurned = "Weight needs to be set to calculate."; }
 				else {
 					$this->TotalCaloriesBurned = $this->TotalCaloriesBurned + $caloriesBurned;
 				}
-				
+
 				// calculate personal records
 				if($distance > $this->LongestDistance){ $this->LongestDistance = $distance; }
 				if($time > $this->LongestTime){ $this->LongestTime = gmdate("H:i:s", $time); }
 				if($caloriesBurned > $this->MostCalories){ $this->MostCalories = $caloriesBurned; }
 				if(($time/$distance) < $this->QuickestPace){ $this->QuickestPace = gmdate("i:s",($time/$distance)); }
 			}
-			
-			
-			
+
+
+
 			// calculate averages
 			$this->AverageDistance = number_format($this->TotalDistance / $this->TotalNumberOfRuns, 1);
 			$this->AverageTime = gmdate("H:i:s", $this->TotalTime / $this->TotalNumberOfRuns);
@@ -93,27 +93,27 @@ class UserRuns {
 		catch (PDOException $e) {
 			die( $e->getMessage() );
 			return null;
-		}	
+		}
 	}
-	
+
 	// returns an array of miles with key set as date timestamp and the value as miles
 	// every day without a run will still have an entry but mileage will be at 0
 	function getData($startDate, $endDate, $data) {
 		try {
 			$user = new User(getUserID($_COOKIE['User']));
-			
+
 			date_default_timezone_set('America/New_York');
-			
+
 			$dateDiff = floor(($endDate-$startDate)/(60*60*24));
 			$interval = array();
-			
+
 			// add dates as keys and 0 each distance out in interval array
 			for($i = 0; $i <= $dateDiff; ++$i) {
 				$days = '+' . $i . ' days';
 				$date = date('Y-m-d',strtotime($days, $startDate));
 				$interval[$date] = 0;
 			}
-			
+
 			// for each run, if it's in the interval,
 			// add the distance to the corresponding date key
 			foreach($this->runs as $run) {
@@ -132,9 +132,9 @@ class UserRuns {
 		catch (PDOException $e) {
 			die( $e->getMessage() );
 			return null;
-		}	
+		}
 	}
-	
+
 	function createRun($date, $distance, $time, $terrain = "", $difficulty ="", $conditions="", $temperature="", $timeOfDay = "", $comments="") {
 				//create record in rruns
 		try {
@@ -190,7 +190,7 @@ class UserRuns {
 			return null;
 		}
 	}
-	
+
 	function deleteRun($RunID){
 		$connString = "mysql:host=localhost;dbname=knovak18";
 		$user = "knovak18";
@@ -198,20 +198,20 @@ class UserRuns {
 
 		$pdo = new PDO($connString,$user,$pass);
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		
+
 		// delete from rrusers
 		$sql = "DELETE FROM rrruns
 				WHERE RunID=" . $RunID . "";
 
 		$statement = $pdo->prepare($sql);
 		$statement->execute();
-		
+
 		// delete from rruserruns
 		$sql2 = "DELETE FROM rruserruns
 				 WHERE RunID=" . $RunID . "
 				 AND  UserID=" . $this->UserID . "";
 	}
-	
+
 	function getTotalDistance() { return number_format($this->TotalDistance, 1);}
 	function getTotalNumberOfRuns() { return $this->TotalNumberOfRuns;}
 	function getTotalTime() {return gmdate("H:i:s", $this->TotalTime);}
@@ -245,13 +245,13 @@ function calculateCaloriesBurned($age, $weight, $gender, $time, $heartRate = "")
 							   *($minutes / 4.184);
 		}
 		else {
-			$caloriesBurned = (($age * 0.074) - ($weight * 0.05741) 
+			$caloriesBurned = (($age * 0.074) - ($weight * 0.05741)
 							   +($heartRate * 0.4472) - 20.4022)
 							   *($minutes/4.184);
 		}
 		return number_format($caloriesBurned, 0);
 	}
-	
+
 }
 
 function getHeartRate($age){
@@ -285,8 +285,8 @@ function getHeartRate($age){
 	if($age >= 71){
 		$heartRate = 101.5;
 	}
-	
+
 	return $heartRate;
-} 
+}
 
 ?>
