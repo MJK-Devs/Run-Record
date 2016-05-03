73e9071b7c0b803a7257cc364a61f60a404acc27
Matt Boehlke <matthew.boehlke@gmail.com>
2016-05-02 23:38:49 -0400

    Fixed dropdown

--- Project/record.php
+++ Project/record.php
@@ -2,8 +2,6 @@
 
 <html>
 <head>
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<title>Record Run - Record</title>
 	<?php include("includes/header.php"); ?>
 </head>
@@ -67,13 +65,15 @@
 <div class="col-md-2"></div>
 </div>
 
-
-
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+
+
 
 </body>
 </html>
