15095dc033cd7a181842803a1782885f12b7e90d
Kevin Novak <kevinmnovak@yahoo.com>
2016-05-02 22:40:09 -0400

    Changed profile panels to be blue instead of purple.

--- Project/profile/edit.php
+++ Project/profile/edit.php
@@ -16,7 +16,7 @@
 	} //else { $username = ""; }
 	?>
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-	   <div class="panel panel-info">
+	   <div class="panel panel-primary">
 		  <div class="panel-heading">
 			<h3 class="panel-title"><?php echo $user->getUsername(); ?></h3>
 		  </div>
