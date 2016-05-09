4ebe06ccd0b69c634c2d4d12b32c4e8175ebe37d
jaason435 <jaason435@gmail.com>
2016-05-08 22:01:07 -0400

    Removed green success banner from edit profile page
    Also re-added default.png to images

--- Project/profile/edit.php
+++ Project/profile/edit.php
@@ -25,7 +25,6 @@
 	} //else { $username = ""; }
 	?>
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
-	   <?php echo $_SESSION["uploadSuccess"]; ?>
 	   <div class="panel panel-primary">
 		  <div class="panel-heading">
 			<h3 class="panel-title"><?php echo $user->getUsername(); ?></h3>
